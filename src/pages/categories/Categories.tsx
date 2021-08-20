5f05b63f6da22f86340b9588a0aa68070659cb4c
Jose Alejandro Lopez Ariza <lopezarizajosealejandro@gmail.com>
2021-08-19 22:25:54 -0500

    Carousel added

--- src/pages/categories/Categories.tsx
+++ src/pages/categories/Categories.tsx
@@ -1,9 +1,13 @@
-import CategoryHeader from "./components/CategoryHeader";
+
+import CategoryHeader from './components/CategoryHeader';
 
 export default function Categories() {
   return (
     <div className="w-4/12 mx-auto p-2">
-      <CategoryHeader image="https://mitienda.moda/img/productos-de-entrega-inmediata.9ad72bec.jpg" showMore />
+      <CategoryHeader
+        image="https://mitienda.moda/img/productos-de-entrega-inmediata.9ad72bec.jpg"
+        showMore
+      />
     </div>
   );
 }
