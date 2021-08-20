8234c9a91453829415c61d16845507f7646398b1
Jose Alejandro Lopez Ariza <lopezarizajosealejandro@gmail.com>
2021-08-20 18:24:12 -0500

    Fixed

--- src/core/layout/FooterBar.tsx
+++ src/core/layout/FooterBar.tsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom';
 
 export default function FooterBar(props: any) {
   return (
-    <div className="flex justify-center sticky bottom-0 left-0 bg-white shadow-lg z-10 w-full h-14">
+    <div className="flex justify-center fixed bottom-0 left-0 bg-white shadow-lg z-10 w-full h-14">
       <NavLink to="" activeClassName="text-red-400" exact>
         <button className="mx-10">
           <i className="fas fa-home text-xl"></i>
